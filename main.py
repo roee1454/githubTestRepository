b7896916f253581fe35dfec4cfd261ed29713a99
roee1454 <66238130+roee1454@users.noreply.github.com>
2022-09-03 14:59:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import string, typing, time
+
+class Game:
+    def __init__(self, char, time_elapsed):
+        self.char = char
+        self.char = [_ for _ in string.ascii_letters]
+        self.time_elapsed = time_elapsed
+        self.time_elapsed  = time.time()
+        
