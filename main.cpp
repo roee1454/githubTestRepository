86f3af47dc5fc98a4d634b9f51b9e35e134f65a4
roee1454 <66238130+roee1454@users.noreply.github.com>
2022-09-03 23:18:16 +0300

    wrtie code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+std::string reversed(std::string);
+
+int main(){
+    std::string str;
+    std::cout<<"Give me a string: " << std::endl;
+    std::cin>>str;
+    std::cout<<"The reversed string:\n"<<reversed(str)<<std::endl;
+}
+std::string reversed(std::string str){
+    std::string new_str;
+    std::cout<<str.length()<<std::endl;
+    for (int i = str.length(); i>str.length(); i--){
+        char sch = str[i];
+    }
+    return new_str;
+}
